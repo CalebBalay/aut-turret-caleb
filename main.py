21bf4eae1881e49f676af238c0be6dadfe342b18
CalebBalay <cbalay@tamu.edu>
2024-09-28 15:52:45 -0500

    Added distance and angle calculation functions; added variables to get camera width and height.

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from facial_detection import ImageDetection
 import cv2
+import time
+from math import *
+
+
+def get_difference(width, height, ex, ey):
+    return sqrt((ex - width / 2) ** 2 + (ey - height / 2) ** 2)
+def get_angle(width, height, ex, ey):
+    return arcsin(ey / get_difference(width, height, ex, ey))
 
 camera = cv2.VideoCapture(0)
 detector = ImageDetection()
+width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 while 1:
     ret, img = camera.read()
     face = ImageDetection.detect_face(detector, img)
-
+    
+    '''for (x,y,w,h) in face:
+        # To draw a rectangle in a face 
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+    cv2.rectangle(img, (int(width / 2), int(height / 2)), (int(width / 2 + 10), \
+                  int(height / 2 + 10)),\
+                  (255,255,0),2) 
+    if len(face) != 0:
+        print(face)
+        print(f"distance is {get_difference(width, height, face[0][0] + , face[0][1])}")'''
     cv2.imshow('img', img)
-
-    print(face)
-
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
